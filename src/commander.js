8e1fd4a271ec2455e650bb48845035d6b44c926f
fwlpe <arestovfa@gmail.com>
2018-07-16 22:22:13 +0300

    missing semicolon fixed =]

--- src/commander.js
+++ src/commander.js
@@ -5,4 +5,4 @@ export default commander
   .command('gendiff <firstConfig> <secondConfig>')
   .option('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'Output format')
-  .description('Compares two configuration files and shows a difference.')
+  .description('Compares two configuration files and shows a difference.');
