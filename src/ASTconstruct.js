e346743a20f4110e365f94355f09c6b6b470ea1d
fwlpe <arestovfa@gmail.com>
2018-07-23 18:51:33 +0300

    stage 5 fixes

--- src/ASTconstruct.js
+++ src/ASTconstruct.js
@@ -2,26 +2,31 @@ import _ from 'lodash';
 
 const astConstruct = (obj1, obj2) => {
   const arrOfUniqKeys = _.union(Object.keys(obj1), Object.keys(obj2));
-  const renderIter = (acc, key) => {
+  const renderIter = (key) => {
     // const obj1[key] = obj1[key];
     // const obj2[key] = obj2[key];
     // const values = { value1: obj1[key], value2: obj2[key] };
     if (!_.has(obj1, key)) {
-      return { ...acc, [key]: { difference: 'added', value2: obj2[key] } };
+      return { key, type: 'added', value2: obj2[key] };
     }
     if (!_.has(obj2, key)) {
-      return { ...acc, [key]: { difference: 'removed', value1: obj1[key] } };
+      return { key, type: 'removed', value1: obj1[key] };
     }
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
       const children = astConstruct(obj1[key], obj2[key]);
-      return { ...acc, [key]: { difference: 'objects', children } };
+      return { key, type: 'objects', children };
     }
     if (obj1[key] === obj2[key]) {
-      return { ...acc, [key]: { difference: 'same', value1: obj1[key] } };
+      return { key, type: 'same', value1: obj1[key] };
     }
-    return { ...acc, [key]: { difference: 'different', value1: obj1[key], value2: obj2[key] } };
+    return {
+      key,
+      type: 'different',
+      value1: obj1[key],
+      value2: obj2[key],
+    };
   };
-  return arrOfUniqKeys.reduce(renderIter, {});
+  return arrOfUniqKeys.map(renderIter);
 };
 
 export default astConstruct;
--- src/renderers/renderObjLike.js
+++ src/renderers/renderObjLike.js
@@ -10,35 +10,37 @@ const renderValue = (value, deeplvl) => {
   return ['{', ...renderedArr, `${indentation}}`].join('\n');
 };
 
-const renderObjLike = (ast, deeplvl = 0) => {
-  const astKeys = Object.keys(ast);
+const prerenderArr = (ast, deeplvl = 0) => {
+  // const astKeys = Object.keys(ast);
   const indentation = '    '.repeat(deeplvl);
-  const renderIter = (key) => {
-    const astNode = ast[key];
-    const { difference } = astNode;
+  const renderIter = (astNode) => {
+    // const astNode = ast[key];
+    const { type, key } = astNode;
     const nodeValue1 = renderValue(astNode.value1, deeplvl + 1);
     const nodeValue2 = renderValue(astNode.value2, deeplvl + 1);
     // const keyInObj1Removed = `${indentation}  - ${key}: ${nodeValue1}`;
     // const keyInObj2Added = `${indentation}  + ${key}: ${nodeValue2}`;
     // const equalValuesCase = `${indentation}    ${key}: ${nodeValue1}`;
-    switch (difference) {
+    switch (type) {
       case 'same':
         return `${indentation}    ${key}: ${nodeValue1}`;
       case 'different':
-        return `${indentation}  - ${key}: ${nodeValue1}\n${indentation}  + ${key}: ${nodeValue2}`;
+        return [`${indentation}  - ${key}: ${nodeValue1}`, `${indentation}  + ${key}: ${nodeValue2}`];
       case 'added':
         return `${indentation}  + ${key}: ${nodeValue2}`;
       case 'removed':
         return `${indentation}  - ${key}: ${nodeValue1}`;
       case 'objects':
-        return `${indentation}    ${key}: ${renderObjLike(astNode.children, deeplvl + 1)}`;
+        return [`${indentation}    ${key}: {`, prerenderArr(astNode.children, deeplvl + 1), `${indentation}    }`];
       default:
-        throw new Error(`unknown difference: ${difference}. internal bug`);
+        throw new Error(`unknown type: ${type}. internal bug`);
     }
   };
-  const renderedArr = astKeys.map(renderIter);
-  const renderedStr = ['{', ...renderedArr, `${indentation}}`].join('\n');
-  return renderedStr;
+  const renderedArr = ast.map(renderIter);
+  // const renderedStr = _.flattenDeep(renderedArr).join('\n');
+  return renderedArr;
 };
+
+const renderObjLike = ast => _.flattenDeep(['{', prerenderArr(ast), '}']).join('\n');
 
 export default renderObjLike;
