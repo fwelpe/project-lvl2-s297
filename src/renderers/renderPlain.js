ecc24973d7e467514709b36d01816d594d81b6c5
fwlpe <arestovfa@gmail.com>
2018-07-23 19:30:26 +0300

    final. minor fixes

--- src/renderers/renderPlain.js
+++ src/renderers/renderPlain.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const complexCheck = value => (_.isObject(value) ? 'complex value' : `'${value}'`);
+const stringify = value => (_.isObject(value) ? 'complex value' : `'${value}'`);
 
 const prerenderArr = (ast, parentArr = []) => {
   const renderIter = (astNode) => {
@@ -11,11 +11,11 @@ const prerenderArr = (ast, parentArr = []) => {
       case 'same':
         return `${beginning} was the same.`;
       case 'added':
-        return `${beginning} was added with ${complexCheck(astNode.value2)}`;
+        return `${beginning} was added with ${stringify(astNode.value2)}`;
       case 'removed':
         return `${beginning} was removed.`;
       case 'different':
-        return `${beginning} was updated. From ${complexCheck(astNode.value1)} to ${complexCheck(astNode.value2)}`;
+        return `${beginning} was updated. From ${stringify(astNode.value1)} to ${stringify(astNode.value2)}`;
       case 'objects':
         return prerenderArr(astNode.children, fullKeyArr);
       default:
